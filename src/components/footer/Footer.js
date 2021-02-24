1b515188553f9d70287f2e5a444af55e48f5b0a4
Gabriela Nicolau <gabriella.nicolau@gmail.com>
2021-02-24 18:16:53 -0500

    changed footer text

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,9 +9,9 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made by Saad Pasta | Modified by David Rakosi @CleverProgrammer")}
-        </p>
+        {/*<p className={isDark ? "dark-mode footer-text" : "footer-text"}>*/}
+        {/*  {emoji("Made by Saad Pasta | Modified by Gabriela Nicolau")}*/}
+        {/*</p>*/}
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a href="https://github.com/saadpasta/developerFolio">
